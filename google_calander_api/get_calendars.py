bd134fc863ae440308da63100f9134e689b5a455
Itay Benbenisti <itay.benben@gmail.com>
2022-03-12 17:34:22 +0200

    Get google calander events api function added

---
+++ google_calander_api/get_calendars.py
@@ -0,0 +1,33 @@
+from pydantic import Json
+from .cal_setup import get_calendar_service
+from configparser import ConfigParser
+
+# Read config.ini file
+config_obj = ConfigParser()
+config_obj.read("google_calander_api\config.ini")
+print(config_obj.sections())
+
+calanderid = config_obj["googleCalander"]["calendarId"]
+
+
+def get_cal_list():
+    """get list of all user's calanders
+
+    Returns:
+        list: list of calanders metadata.
+    """
+    service = get_calendar_service()
+    calendars_result = service.calendarList().list().execute()
+    calendars = calendars_result.get('items', [])
+    return calendars
+
+
+def get_cal_events():
+    """return list of all calander's events.
+
+    Returns:
+        list: all calander events as jsons.
+    """
+    service = get_calendar_service()
+    events = service.events().list(calendarId=calanderid).execute()
+    return events["items"]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from pip import main
 from fastapi import Depends, FastAPI
-
+import google_calander_api
 
 app = FastAPI()
 
@@ -7,4 +8,8 @@ app = FastAPI()
 async def testapp():
     return {"i am running": True}
 
-
+@app.post("/updateCalander")
+async def update_calander():
+    events = google_calander_api.get_cal_events()
+    print(events)
+    #todo: push to mongo with ofek
