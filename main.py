1eebcdb2396e7c106bf382ff8bf0d0388aacc798
Itay Benbenisti <itay.benben@gmail.com>
2022-03-14 23:35:23 +0200

    Getting names of teams, plugot and maarachim api functions added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from re import A
 from urllib import response
 from fastapi import Depends, FastAPI, Body
 import DAL.database as db
@@ -75,6 +76,46 @@ async def get_pluga_list():
     for name in pluga_names_json:
         array.append({"label": name, "value": name})
     return array
+
+
+@app.get("/getListOfPeople/{pluga_name}/maarach", description="Get all maarach names in a pluga")
+async def get_maarach_list_in_pluga(pluga_name: str) -> list:
+    pluga_names = config_obj["pluga"]["names"]
+    pluga_names_json = json.loads(pluga_names)
+    return list(pluga_names_json[pluga_name].keys())
+
+
+@app.get("/getListOfPeople/{pluga_name}/{maarach}/team", description="Get all team names in a maarach")
+async def get_team_list_in_maarach(pluga_name: str, maarach: str) -> list:
+    pluga_names = config_obj["pluga"]["names"]
+    pluga_names_json = json.loads(pluga_names)
+    return pluga_names_json[pluga_name][maarach]
+
+
+@app.get("/getListOfPeople/team", description="Get all team names")
+async def get_team_list() -> list:
+    pluga_names = config_obj["pluga"]["names"]
+    pluga_names_json = json.loads(pluga_names)
+    team_list = []
+    for pluga in pluga_names_json:
+        for maarach in pluga_names_json[pluga]:
+            for team in pluga_names_json[pluga][maarach]:
+                team_list.append(team)
+    return team_list
+
+
+@app.get("/getListOfPeople/all", description="Get all pluga, maarach and team names")
+async def get_name_list() -> list:
+    pluga_names = config_obj["pluga"]["names"]
+    pluga_names_json = json.loads(pluga_names)
+    names_list = set()
+    for pluga in pluga_names_json:
+        names_list.add(pluga)
+        for maarach in pluga_names_json[pluga]:
+            names_list.add(maarach)
+            for team in pluga_names_json[pluga][maarach]:
+                names_list.add(team)
+    return names_list
 
 
 @app.get("/getcalander/{pluga_name}", description="Get spesific pluga's schedule")
