d0f805e70285ce184547448b91cb35d785527a8f
ofekshlin <ofekshlin@gmail.com>
2022-03-16 21:25:25 +0200

    fixed the update calander bag

--- DAL/database.py
+++ DAL/database.py
@@ -1,5 +1,6 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
+from pymongo import InsertOne
 
 
 MONGO_DETAILS = "mongodb://admin:wowisofek@wowmongo.eastus.azurecontainer.io:27017"
@@ -39,15 +40,21 @@ async def retrieve_schedule(schedule):
     return events
 
 async def add_event(event: dict, schedule: str) -> dict:
-    events_collection = database.get_collection(schedule)
+    events_collection = await get_or_create_collection(schedule) 
     event = await events_collection.insert_one(event)
     new_event = await events_collection.find_one({"_id": event.inserted_id})
     return event_helper(new_event)
 
 async def add_events(events: dict, schedule: str):
-    new_collection = database.create_collection(schedule)
-    events_collection = database.get_collection(schedule)   
-    events_request = await events_collection.bulk_write(events)
+    events_collection = await get_or_create_collection(schedule)
+    requests = [InsertOne(e) for e in events] 
+    events_request = await events_collection.bulk_write(requests)
     if events_request.inserted_count == len(events):
-        return events
+        return "Succesed"
     return "Faild"
+
+async def get_or_create_collection(collection: str):
+    collection_names = await database.list_collection_names()
+    if collection not in collection_names:
+        await database.create_collection(collection)  
+    return database.get_collection(collection)
